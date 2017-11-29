0a54f3a9b7e6bef4ba8817cadbedb06f5b64d732
durban9 <32137923+durban9@users.noreply.github.com>
2017-11-29 03:23:52 -0600

    Add files via upload
    This is the Annuity half of the Financials.zip file, which calculates an annuity.

---
+++ Annuity.java
@@ -0,0 +1,19 @@
+
+package business;
+
+/**
+ *
+ * @author DUrban
+ */
+public class Annuity {
+    public static final String AMTDESC="Monthly Deposit";
+    public static final String RESDESC="Annual Interest Rate";
+    public static final String term="Term";
+
+    private double monthlyDepositValue;
+    private double annualInterestRateValue;
+    private double termValue;
+}
+
+
+
